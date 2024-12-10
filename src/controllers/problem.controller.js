2c1f5a8d23b9aaa3478d195182dd7053776d0929
zealrods <zealrodrigues@gmail.com>
2024-12-10 15:08:55 +0530

    Feat: added unimplemented error for api contract

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -3,23 +3,33 @@ function pingProblemController(req,res){
 }
 
 function addProblem(req,res){
-
+    return res.status(501).json({
+        message: 'Not Implemented'
+    });
 }
 
 function getProblem(req,res){
-
+    return res.status(501).json({
+        message: 'Not Implemented'
+    });
 }
 
 function getProblems(req,res){
-
+    return res.status(501).json({
+        message: 'Not Implemented'
+    });
 }
 
 function deleteProblem(req,res){
-
+    return res.status(501).json({
+        message: 'Not Implemented'
+    });
 }
 
 function updateProblem(req,res){
-
+    return res.status(501).json({
+        message: 'Not Implemented'
+    });
 }
 
 module.exports = {
